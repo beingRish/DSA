c5677d8b1b1c47bae155b358288918d7bb64d3dd
Rishabh Singh <rishabhjuly11@gmail.com>
2023-06-11 13:21:37 +0530

    learnt bubble sort

---
+++ DSA/BubbleSort.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+void bubbleSort(int arr[], int n)
+{   
+    for(int i = 1; i < n; i++){
+        // for round 1 to n-1
+
+        bool swapped = false;
+
+        for(int j = 0; j < n-i; j++){
+
+            // process element till n-i th index
+            
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+        }
+        if(swapped == false){
+            break;
+        }
+    }
+}
+
+void sortedArray(int arr[], int n){
+
+    for(int i=0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+}
+
+int main(){
+    
+    int arr[5] = {6, 2, 8, 4, 10};
+
+    bubbleSort(arr, 5);
+    sortedArray(arr, 5);
+
+}
