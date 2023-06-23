5dcf992990723c1a418e30f6ee6f8424f541bb7b
Rishabh Singh <rishabhjuly11@gmail.com>
2023-06-23 20:33:39 +0530

    Learnt Recursion with easiest way

--- C++/Phase-2/Lecture32(Recursion-2).cpp
+++ C++/Phase-2/Lecture32(Recursion-2).cpp
@@ -33,18 +33,54 @@ int fib(int n){
     return ans;
 }
 
+void sayDigit(int n, string arr[]) {
+
+    // base case
+    if(n == 0)
+        return ;
+
+    // processing
+    int digit = n % 10;
+    n = n / 10;
+
+    // recursive call
+    sayDigit(n, arr);
+
+    cout << arr[digit] << " ";
+
+}
+
 int main(){
+/*  // **************************** Easiest Way to understand Recurstion *********************
 
     int dest = 10;
     int src = 1;
 
     reachHome(src, dest);
+*/
 
     // **************************** Fibonnacci No *******************************************
-
+/*
     int n;
     cout << "Enter a number: ";
     cin >> n;
 
     cout << n << "th term of fibonacci series is " << fib(n);
+*/
+
+    string arr[10] = {"zero", "one", "two", "three", "four", "five",
+                        "six", "seven", "eight", "nine"};
+
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    cout << endl << endl << endl;
+    sayDigit(n, arr);
+    cout << endl << endl << endl;
+
+
+
+
+    return 0;
 }
