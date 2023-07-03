9ea598bf250590fc5077d5aa52192840ea1f991a
Rishabh Singh <rishabhjuly11@gmail.com>
2023-07-03 13:37:48 +0530

    doubly linked list

--- DSA/Phase-2/linked List/singlyLinkedList.cpp
+++ DSA/Phase-2/linked List/singlyLinkedList.cpp
@@ -47,7 +47,7 @@ void print(Node* &head) {
     Node* temp = head;
 
     while( temp != NULL ) {
-        cout << temp -> data;
+        cout << temp -> data << " ";
         temp = temp -> next;
     }
     cout << endl;
